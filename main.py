4b3b6b5d55e335c49faded072cf7b77555e221f5
Luna Lunapiena <lunacodes@gmail.com>
2015-10-29 03:35:31 -0400

    Databse Is Functional.  All Functions Work Properly

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+#!/usr/bin/python
+
+#Note: Replace String Queries with Variables for Security Reasons
+#Add an Undo Delete Function - This would involve a backup Database
+
 import sys
+import sqlite3 as lite
+
+con = lite.connect('mood_history.sqlite')
+
+
 
 """Add User Profiles???"""
 """Deleted Colors from this version due to Dev Glitches"""
@@ -14,88 +24,86 @@ def menu():
     if menu_choice == "1":
         log_mood(date, time)
     elif menu_choice == "2":
-        view_history(entry_log)
+        view_history()
     elif menu_choice == "3":
         quote_of_the_day()
     elif menu_choice =="4":
         delete_log_entry()
     elif menu_choice =="5":
-        delete_history()
+        delete_entire_history()
     elif menu_choice == "6":
         quit()
 
 def log_mood(date, time):
-    """Logs the User's Mood & Saves to a Global"""
-    global log_number
-    global entry_log
-
-    log_number += 1
-
-    print("") #This is here for Proper Spacing
+    """Logs the User's Mood & Saves to Database"""
+    print("") #Spacing
 
     mood = input("What is your Mood?\n")
     intensity = input("On a Scale of 1-10, How Intense is your Mood?\n")
     notes = input("Wanna add any Notes about this?\n")
 
-    temp_entry_log = date + "  |  " + time + "  |  " +  mood + "  |  " +  intensity + "  |  " + notes + "\n"
-    # temp_entry_log = str(log_number) + ".  " + date + "  |  " + time + "  |  " +  mood + "  |  " +  intensity + "  |  " + notes + "\n"
-
-    with open('mood_history.txt', 'a') as f: #Consider Hopping Back to JSON For Data Dump
-        f.write(temp_entry_log)
-        f.close()
-
+    con.execute("INSERT INTO MOOD_HISTORY_LOG (LOG_DATE, MOOD, INTENSITY, NOTES) \
+        VALUES (?, ?, ?, ?)", (date, mood, intensity, notes));
+    con.commit()
+    print("Data Logged")
     print("\n")
 
 
-def view_history(entry_log):
-    """Allows the user to view their History in Daily, Weekly, Monthly & Yearly Intervals"""
-    global log_number
+def view_history():
+    "Views User's Mood History from Database"
+    #Add in Pagination
+
     print("") #Spacing
-    print("Date      Time    Mood   Intensity  Notes")
-    with open('mood_history.txt', 'r') as f:
-        output = f.read()
-    print(output)
+
+    cur.execute("SELECT * FROM MOOD_HISTORY_LOG")
+    for row in cur:
+        print("Log Number:", row[0])
+        print("Date: ", row[1])
+        print("Mood: ", row[2])
+        print("Intensity: ", row[3])
+        print("Time: ", row[4], "\n")
 
 
 def quote_of_the_day(): #Maybe we can Align the Text to Center??
     """Pulls quotes from a Giant Text File & Display One at Random"""
     """Does it make sense to write this in JSON?  Is it reasonable to pull a dictionary or list from JSON for this???"""
 
-    quote_str = "I find hope in the darkest of days, and focus in the brightest. I do not judge the universe."
-    author_str = "-Tenzin Gyatsu: Dalai Lama"
-    print("") #Spacing
-    print(quote_str.center(len(quote_str))) 
-    print(author_str.center(len(author_str)) + "\n")
-
-    # print("I find hope in the darkest of days, and focus in the brightest. I do not judge the universe.") 
-    # print("-Tenzin Gyatso: Dalai Lama \n")
+    cur.execute("SELECT * FROM QUOTES ORDER BY RANDOM() LIMIT 1;")
+    for row in cur:
+        print(row[1], "\n")
+        print(row[2], "\n")
 
 
 def delete_log_entry():
-    print("This Feature is in Revision & will become available next version")
-    # user_log_to_delete = input("Enter the number of the line you wish to delete: \n")
-    # with open('mood_history.txt', 'r') as log_deletetion_input:
-    #     with open('mood_history.txt', 'w') as log_deletion_output:
-    #         for line in log_deletetion_input:
-    #             if line.startswith(user_log_to_delete) == False:
-    #                 output.write("This Is A Test") ## The problem here is that it's overwriting from the beginning of the file
-    #                 I will need to consult ppl about this
-
-
-    # input('Enter the Number of the log you wish to delete.  You may type "main" instead to return to the Main Menu')
     """This will have to list all the logs & be able to modify them via their list position"""
+    #This doesn't adjust the numbers in ID - I may just have to live with that ...
 
+    print("")
+    log_number_to_delete = int(input('Enter the number of the Log you wish to delete.  If you don\'t know the number, press "v" to View log, or "m" for Main Menu \n\n'))
+    # log_number_to_delete = int(log_number_to_delete)
+    if log_number_to_delete == "v":
+        view_history()
+    elif log_number_to_delete == "m":
+        pass
+    else:
+        cur.execute("SELECT * FROM MOOD_HISTORY_LOG")
+        for row in cur:
+            cur.execute("DELETE FROM MOOD_HISTORY_LOG WHERE ID=?;", (log_number_to_delete,))
+            con.commit()
+        
+        print("Log %d Deleted!" % log_number_to_delete, "\n")
 
-def delete_history():
+def delete_entire_history():
     user_delete_decision = input("Are you sure you want to do this???  (Y/N) \n")
 
     """Add in a Password protection here"""
     if user_delete_decision[0].lower() == "y": 
-        with open('mood_history.txt', "w") as f:
-            f.write("")
-            f.close()
-        print("\nDeleted\n")
-        log_number = 0
+        cur.execute("DELETE FROM MOOD_HISTORY_LOG WHERE ID IN (SELECT ID FROM MOOD_HISTORY_LOG)")
+        con.commit()
+        
+        print("")
+        print("Mood History Deleted\n")
+
     else:
         print("Nevermind.  Back to the Main Menu")
 
@@ -109,14 +117,25 @@ def quit():
 
 
 if __name__ == "__main__":
-    #Add In A Check To see if mood_history.txt exists.  
-    #If not - Create it
-    
-    entry_log = ""
-    with open('mood_history.txt', 'r') as f:
-        log_number = sum(1 for line in f) 
-    print(log_number)
+    con = lite.connect('mood_history.sqlite')
 
+    cur = con.cursor()
+    con.execute('''CREATE TABLE IF NOT EXISTS MOOD_HISTORY_LOG (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        LOG_DATE TEXT NOT NULL,
+        MOOD TEXT NOT NULL,
+        INTENSITY INT NOT NULL,
+        NOTES TEXT NOT NULL);''')
+    con.execute('''CREATE TABLE IF NOT EXISTS QUOTES (ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+        QUOTE TEXT NOT NULL,
+        AUTHOR TEXT NOT NULL);''')
+
+    #This is here for Undo purposes
+    #I'll need to find someway of highlighting the differences
+    con.execute('''CREATE TABLE IF NOT EXISTS MOOD_HISTORY_LOG_BACKUP (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        LOG_DATE TEXT NOT NULL,
+        MOOD TEXT NOT NULL,
+        INTENSITY INT NOT NULL,
+        NOTES TEXT NOT NULL);''')
     print("\nHi!!  Welcome to the Friendly Mood Tracker :)\n" )
     print("What would you like to do? (Enter as Number)\n")
 
